941ff507bb4afe2286aba03ca1f1d39be1cf7527
aniketparate <aniketvparate@gmail.com>
2022-08-23 19:16:31 +0530

    added webview tts

---
+++ pdf-viewer/index1.js
@@ -0,0 +1,19 @@
+const speak = (text, voice = 0, speed = 1, volume = 1, pitch = 1) => {
+  let speech = window.speechSynthesis
+  let utterance = new SpeechSynthesisUtterance(text)
+  utterance.voice = speechSynthesis.getVoices()[voice];
+  utterance.rate = speed
+  utterance.pitch = pitch
+  utterance.volume = volume
+  speech.cancel()
+  return speech.speak(utterance)
+}
+
+const btnClick = document.getElementById('click')
+
+btnClick.addEventListener('click', () => {
+  speak("hello world, this is offline text to speech!")
+  console.log('click')
+})
+
+// export default speak
